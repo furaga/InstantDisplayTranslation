c9ea17eeae87ac334279fd78fbf6f995c831fb60
unknown <furaga>
2014-07-19 20:34:32 +0900

    first commit

---
+++ InstantDisplayTranslation/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace InstantDisplayTranslation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labe = new System.Windows.Forms.Label();
+            this.shiftCheckbox = new System.Windows.Forms.CheckBox();
+            this.altCheckbox = new System.Windows.Forms.CheckBox();
+            this.ctrlCheckbox = new System.Windows.Forms.CheckBox();
+            this.keyCombobox = new System.Windows.Forms.ComboBox();
+            this.Reflesh = new System.Windows.Forms.Button();
+            this.enableCheckBox = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.labe);
+            this.splitContainer1.Panel1.Controls.Add(this.shiftCheckbox);
+            this.splitContainer1.Panel1.Controls.Add(this.altCheckbox);
+            this.splitContainer1.Panel1.Controls.Add(this.ctrlCheckbox);
+            this.splitContainer1.Panel1.Controls.Add(this.keyCombobox);
+            this.splitContainer1.Panel1.Controls.Add(this.Reflesh);
+            this.splitContainer1.Panel1.Controls.Add(this.enableCheckBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.canvas);
+            this.splitContainer1.Size = new System.Drawing.Size(890, 422);
+            this.splitContainer1.SplitterDistance = 307;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 12);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Then, push the following shortcut key (can be modified)";
+            // 
+            // labe
+            // 
+            this.labe.AutoSize = true;
+            this.labe.Location = new System.Drawing.Point(12, 9);
+            this.labe.Name = "labe";
+            this.labe.Size = new System.Drawing.Size(278, 12);
+            this.labe.TabIndex = 30;
+            this.labe.Text = "Move mouse cursor to a corner or a side of a display";
+            // 
+            // shiftCheckbox
+            // 
+            this.shiftCheckbox.AutoSize = true;
+            this.shiftCheckbox.Location = new System.Drawing.Point(102, 55);
+            this.shiftCheckbox.Name = "shiftCheckbox";
+            this.shiftCheckbox.Size = new System.Drawing.Size(48, 16);
+            this.shiftCheckbox.TabIndex = 29;
+            this.shiftCheckbox.Text = "Shift";
+            this.shiftCheckbox.UseVisualStyleBackColor = true;
+            this.shiftCheckbox.CheckedChanged += new System.EventHandler(this.shiftCheckbox_CheckedChanged);
+            // 
+            // altCheckbox
+            // 
+            this.altCheckbox.AutoSize = true;
+            this.altCheckbox.Location = new System.Drawing.Point(151, 55);
+            this.altCheckbox.Name = "altCheckbox";
+            this.altCheckbox.Size = new System.Drawing.Size(39, 16);
+            this.altCheckbox.TabIndex = 28;
+            this.altCheckbox.Text = "Alt";
+            this.altCheckbox.UseVisualStyleBackColor = true;
+            this.altCheckbox.CheckedChanged += new System.EventHandler(this.altCheckbox_CheckedChanged);
+            // 
+            // ctrlCheckbox
+            // 
+            this.ctrlCheckbox.AutoSize = true;
+            this.ctrlCheckbox.Location = new System.Drawing.Point(54, 55);
+            this.ctrlCheckbox.Name = "ctrlCheckbox";
+            this.ctrlCheckbox.Size = new System.Drawing.Size(43, 16);
+            this.ctrlCheckbox.TabIndex = 27;
+            this.ctrlCheckbox.Text = "Ctrl";
+            this.ctrlCheckbox.UseVisualStyleBackColor = true;
+            this.ctrlCheckbox.CheckedChanged += new System.EventHandler(this.ctrlCheckbox_CheckedChanged);
+            // 
+            // keyCombobox
+            // 
+            this.keyCombobox.FormattingEnabled = true;
+            this.keyCombobox.Location = new System.Drawing.Point(196, 53);
+            this.keyCombobox.Name = "keyCombobox";
+            this.keyCombobox.Size = new System.Drawing.Size(105, 20);
+            this.keyCombobox.TabIndex = 26;
+            this.keyCombobox.Text = "F1";
+            this.keyCombobox.SelectedIndexChanged += new System.EventHandler(this.keyCombobox_SelectedIndexChanged);
+            // 
+            // Reflesh
+            // 
+            this.Reflesh.Location = new System.Drawing.Point(11, 165);
+            this.Reflesh.Name = "Reflesh";
+            this.Reflesh.Size = new System.Drawing.Size(132, 23);
+            this.Reflesh.TabIndex = 3;
+            this.Reflesh.Text = "Reload real screens";
+            this.Reflesh.UseVisualStyleBackColor = true;
+            this.Reflesh.Click += new System.EventHandler(this.Reflesh_Click);
+            // 
+            // enableCheckBox
+            // 
+            this.enableCheckBox.AutoSize = true;
+            this.enableCheckBox.Checked = true;
+            this.enableCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableCheckBox.Location = new System.Drawing.Point(12, 137);
+            this.enableCheckBox.Name = "enableCheckBox";
+            this.enableCheckBox.Size = new System.Drawing.Size(58, 16);
+            this.enableCheckBox.TabIndex = 0;
+            this.enableCheckBox.Text = "Enable";
+            this.enableCheckBox.UseVisualStyleBackColor = true;
+            this.enableCheckBox.CheckedChanged += new System.EventHandler(this.enableCheckBox_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(3, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 21);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Virtual screen arrangements";
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(579, 422);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.Resize += new System.EventHandler(this.canvas_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 422);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "InstantDisplayTranslation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.CheckBox enableCheckBox;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button Reflesh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labe;
+        private System.Windows.Forms.CheckBox shiftCheckbox;
+        private System.Windows.Forms.CheckBox altCheckbox;
+        private System.Windows.Forms.CheckBox ctrlCheckbox;
+        private System.Windows.Forms.ComboBox keyCombobox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ InstantDisplayTranslation/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FLib;
+
+namespace InstantDisplayTranslation
+{
+    public partial class Form1 : Form
+    {
+        Hooker hooker = new Hooker();
+        Rectangle[] virtualScreenBounds = null;
+        Pen pen = new Pen(Brushes.Black);
+        Font font = new Font("Arial", 64);
+        bool enable = false;
+        Point prevCursorPosition = Point.Empty;
+        ShortcutKey shortcutKey = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            prevCursorPosition = Cursor.Position;
+            RefleshVirtualScreenBounds();
+            hooker.OnKeyHook = OnKeyHook;
+            hooker.OnMouseHook = OnMouseHook;
+            hooker.Hook();
+
+            for (int i = 'A'; i <= 'Z'; i++)
+                keyCombobox.Items.Add("" + (char)i);
+            for (int i = 1; i <= 24; i++)
+                keyCombobox.Items.Add("F" + i);
+
+            shortcutKey = GetShortcutKey();
+
+            enable = enableCheckBox.Checked;
+        }
+
+        bool OnKeyHook(int code, WM wParam, KBDLLHOOKSTRUCT lParam, Hooker hooker)
+        {
+            // F1キー
+            if (OnShortcutkey(lParam.vkCode))
+            {
+                var bounds = GetVirtualSecondaryScreenBounds(Cursor.Position);
+                if (!bounds.IsEmpty && virtualScreenBounds.Length >= 2)
+                    virtualScreenBounds[1] = bounds;
+                this.TopMost = true;
+                this.TopMost = false;
+                canvas.Invalidate();
+                return true;
+            }
+            return false;
+        }
+
+        bool OnShortcutkey(int vkCode)
+        {
+            if (shortcutKey == null)
+                return false;
+
+            if (shortcutKey.ctrl && !hooker.onCtrl)
+                return false;
+
+            if (shortcutKey.alt && !hooker.onAlt)
+                return false;
+
+            if (shortcutKey.shift && !hooker.onShift)
+                return false;
+
+            if (shortcutKey.key == "")
+                return false;
+
+            if (shortcutKey.key.Length == 1)
+            {
+                if (char.ToUpper(shortcutKey.key[0]) != vkCode && char.ToLower(shortcutKey.key[0]) != vkCode)
+                    return false;
+            }
+            else
+            {
+                if (shortcutKey.key[0] != 'F')
+                    return false;
+                int idx;
+                if (!int.TryParse(shortcutKey.key.Substring(1), out idx))
+                    return false;
+                if (idx < 1 && 24 < idx)
+                    return false;
+                if (0x70 + idx - 1 != vkCode)
+                    return false;
+            }
+
+            return true;
+        }
+
+        bool OnMouseHook(int code, WM message, IntPtr state, Hooker hooker)
+        {
+            if (enable)
+            {
+                if (message == WM.MOUSEMOVE)
+                {
+                    Point curPt = Cursor.Position;
+                    Point newPt = ToVirtualWorld(curPt, prevCursorPosition);
+                    Cursor.Position = newPt;
+                    prevCursorPosition = newPt;
+                    if (curPt != newPt)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        int GetBoundsIdx(Rectangle[] bounds, Point pt)
+        {
+            for (int i = 0; i < bounds.Length; i++)
+                if (bounds[i].Contains(pt))
+                    return i;
+            return -1;
+        }
+
+        private Point ToVirtualWorld(Point pt, Point prevPt)
+        {
+            var rbounds = GetRealScreenBounds();
+            var vbounds = GetVirtualScreenBounds();
+            if (rbounds == null || vbounds == null || vbounds.Length <= 1 || vbounds.Length != rbounds.Length)
+                return pt;
+
+            int prev = GetBoundsIdx(rbounds, prevPt);
+            if (prev <= -1)
+                return pt;
+
+            int cur = GetBoundsIdx(rbounds, pt);
+
+            // 勝手に他のスクリーンに移動してはいけない
+            if (cur != prev)
+            {
+                pt.X = Math.Min(Math.Max(pt.X, rbounds[prev].Left), rbounds[prev].Right - 1);
+                pt.Y = Math.Min(Math.Max(pt.Y, rbounds[prev].Top), rbounds[prev].Bottom - 1);
+                cur = prev;
+            }
+
+            if (cur <= -1)
+                return pt;
+            Point vPt = new Point(pt.X - rbounds[cur].X + vbounds[cur].X, pt.Y - rbounds[cur].Y + vbounds[cur].Y);
+
+            Point nextPoint = vPt;
+            int next = -1;
+            for (int i = 0; i < vbounds.Length; i++)
+            {
+                if (i == cur)
+                    continue;
+
+                if (vbounds[i].Contains(new Point(vPt.X - 1, vPt.Y)))
+                    nextPoint = new Point(vPt.X - 3, vPt.Y);
+
+                if (vbounds[i].Contains(new Point(vPt.X + 1, vPt.Y)))
+                    nextPoint = new Point(vPt.X + 3, vPt.Y);
+
+                if (vbounds[i].Contains(new Point(vPt.X, vPt.Y - 1)))
+                    nextPoint = new Point(vPt.X, vPt.Y - 3);
+
+                if (vbounds[i].Contains(new Point(vPt.X, vPt.Y + 1)))
+                    nextPoint = new Point(vPt.X, vPt.Y + 3);
+
+                if (nextPoint != vPt)
+                {
+                    next = i;
+                    break;
+                }
+            }
+
+            if (next <= -1)
+                return pt;
+
+            Point newPt = new Point(nextPoint.X - vbounds[next].X + rbounds[next].X, nextPoint.Y - vbounds[next].Y + rbounds[next].Y);
+
+            return newPt;
+        }
+
+        Rectangle GetVirtualSecondaryScreenBounds(Point pt)
+        {
+            const int cornerSize = 50;
+
+            if (Screen.AllScreens.Length <= 1)
+                return Rectangle.Empty;
+
+            var scr1 = Screen.PrimaryScreen.Bounds;
+            int w1 = scr1.Width;
+            int h1 = scr1.Height;
+
+            var scr2 = Screen.AllScreens[1].Bounds;
+            int w2 = scr2.Width;
+            int h2 = scr2.Height;
+
+            if (Screen.PrimaryScreen.Bounds.Contains(pt))
+            {
+                Point pt1 = pt;
+                pt1.X -= scr1.X;
+                pt1.Y -= scr1.Y;
+
+                if (pt1.X <= cornerSize && pt1.Y <= cornerSize) // 左上
+                    return new Rectangle(scr1.X - w2, scr1.Y - h2 * 2 / 3, w2, h2);
+                else if (pt1.X >= w1 - cornerSize && pt1.Y <= cornerSize) // 右上
+                    return new Rectangle(scr1.X + w1, scr1.Y - h2 * 2 / 3, w2, h2);
+                else if (pt1.X <= cornerSize && pt1.Y >= h1 - cornerSize) // 左下
+                    return new Rectangle(scr1.X - w2, scr1.Y + h1 - h2 / 3, w2, h2);
+                else if (pt1.X >= w1 - cornerSize && pt1.Y >= h1 - cornerSize) // 右下
+                    return new Rectangle(scr1.X + w1, scr1.Y + h1 - h2 / 3, w2, h2);
+                else if (pt1.X < cornerSize) // 左
+                    return new Rectangle(scr1.X - w2, scr1.Y, w2, h2);
+                else if (pt1.Y < cornerSize) // 上
+                    return new Rectangle(scr1.X, scr1.Y - h2, w2, h2);
+                else if (pt1.X >= w1 - cornerSize) // 右
+                    return new Rectangle(scr1.X + w1, scr1.Y, w2, h2);
+                else if (pt1.Y >= h1 - cornerSize) // 下
+                    return new Rectangle(scr1.X, scr1.Y + h1, w2, h2);
+            }
+
+            if (Screen.AllScreens[1].Bounds.Contains(pt))
+            {
+                Point pt2 = pt;
+                pt2.X -= scr2.X;
+                pt2.Y -= scr2.Y;
+
+                if (pt2.X <= cornerSize && pt2.Y <= cornerSize) // 左上
+                    return new Rectangle(scr1.X + w1, scr1.Y + h1 - h2 / 3, w2, h2);
+                else if (pt2.X >= w2 - cornerSize && pt2.Y <= cornerSize) // 右上
+                    return new Rectangle(scr1.X - w2, scr1.Y + h1 - h2 / 3, w2, h2);
+                else if (pt2.X <= cornerSize && pt2.Y >= h1 - cornerSize) // 左下
+                    return new Rectangle(scr1.X + w1, scr1.Y - h2 * 2 / 3, w2, h2);
+                else if (pt2.X >= w2 - cornerSize && pt2.Y >= h1 - cornerSize) // 右下
+                    return new Rectangle(scr1.X - w2, scr1.Y - h2 * 2 / 3, w2, h2);
+                else if (pt2.X < cornerSize) // 左
+                    return new Rectangle(scr1.X + w1, scr1.Y, w2, h2);
+                else if (pt2.Y < cornerSize) // 上
+                    return new Rectangle(scr1.X, scr1.Y + h1, w2, h2);
+                else if (pt2.X >= w2 - cornerSize) // 右
+                    return new Rectangle(scr1.X - w2, scr1.Y, w2, h2);
+                else if (pt2.Y >= h1 - cornerSize) // 下
+                    return new Rectangle(scr1.X, scr1.Y - h2, w2, h2);
+            }
+
+            return Rectangle.Empty;
+        }
+
+        private void enableCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            enable = enableCheckBox.Checked;
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            const float scale = 0.1f;
+
+            e.Graphics.Clear(Color.White);
+
+            var bounds = GetScreenBounds();
+            if (bounds == null || bounds.Length <= 0)
+                return;
+
+            float cx = bounds[0].X + bounds[0].Width / 2;
+            float cy = bounds[0].Y + bounds[0].Height / 2;
+            cx *= scale;
+            cy *= scale;
+
+            float ox = e.Graphics.ClipBounds.Width / 2 - cx;
+            float oy = e.Graphics.ClipBounds.Height / 2 - cy;
+
+            for (int i = 0; i < bounds.Length; i++)
+            {
+                float x = bounds[i].X * scale + ox;
+                float y = bounds[i].Y * scale + oy;
+                float w = bounds[i].Width * scale;
+                float h = bounds[i].Height* scale;
+                e.Graphics.FillRectangle(Brushes.Beige, x, y, w, h);
+                e.Graphics.DrawRectangle(pen, x, y, w, h);
+
+                string text = "" + (i + 1);
+                var size = e.Graphics.MeasureString(text, font);
+                float tx = x + w / 2 - size.Width / 2;
+                float ty = y + h / 2 - size.Height / 2;
+                e.Graphics.DrawString(text, font, Brushes.Black, tx, ty);
+            }
+        }
+
+        private void canvas_Resize(object sender, EventArgs e)
+        {
+            canvas.Invalidate();
+        }
+
+        Rectangle[] GetScreenBounds()
+        {
+            return GetVirtualScreenBounds();
+        }
+
+        private void Reflesh_Click(object sender, EventArgs e)
+        {
+            RefleshVirtualScreenBounds();
+        }
+
+        Rectangle[] GetRealScreenBounds()
+        {
+            return Screen.AllScreens.Select(scr => scr.Bounds).ToArray();
+        }
+
+        Rectangle[] GetVirtualScreenBounds()
+        {
+            return virtualScreenBounds;
+        }
+
+        private void RefleshVirtualScreenBounds()
+        {
+            virtualScreenBounds = GetRealScreenBounds();
+        }
+
+        private void ctrlCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            shortcutKey = GetShortcutKey();
+        }
+
+        private void shiftCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            shortcutKey = GetShortcutKey();
+        }
+
+        private void altCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            shortcutKey = GetShortcutKey();
+        }
+
+        private void keyCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            shortcutKey = GetShortcutKey();
+        }
+
+        ShortcutKey GetShortcutKey()
+        {
+            if (keyCombobox.Text.Length == 1)
+            {
+                if (keyCombobox.Text[0] < 'A' || 'Z' < keyCombobox.Text[0])
+                    return null;
+            }
+            return new ShortcutKey(
+                ctrlCheckbox.Checked,
+                shiftCheckbox.Checked,
+                altCheckbox.Checked,
+                keyCombobox.Text);
+        }
+    }
+
+    public class ShortcutKey
+    {
+        public bool ctrl = false;
+        public bool shift = false;
+        public bool alt = false;
+        public string key = "";
+        public ShortcutKey(bool ctrl, bool shift, bool alt, string key)
+        {
+            this.ctrl = ctrl;
+            this.shift = shift;
+            this.alt = alt;
+            this.key = key;
+        }
+    }
+}
